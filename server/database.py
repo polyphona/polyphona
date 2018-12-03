aa47ffc178c3470d49eae7a9b8f56abfe642d485
Selim Tirellil <selim.tirellil@hotmail.fr>
2018-12-03 17:02:07 +0100

    Adding the database.py script

---
+++ server/database.py
@@ -0,0 +1,51 @@
+import sqlite3
+
+if __name__ == "__main__":
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+
+    #If database is empty, create tables
+    cursor.execute('''CREATE TABLE IF NOT EXISTS songs 
+                      (SongID integer primary key not null,
+                      Json text,
+                      User text)''')
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users
+                      (UserName text primary key not null,
+                      Password text)''')
+
+    conn.commit()
+    conn.close()
+
+
+
+def addSong(Json_song, userName):
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO songs (Json, User) VALUES (?,?)", ( Json_song, userName ))
+
+def addUser():
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+
+def getSongByTitle():
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+
+def songsByUser():
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+
+
+def connectUser(userName,password):
+    conn = sqlite3.connect('polyphona_db.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT count(UserName) FROM users WHERE UserName=?",userName)
+    if cursor.fetchone()[0] == 0:
+        print(1)
+        #throw exception username doesn't exist
+    else :
+        cursor.execute("SELECT Password FROM users WHERE UserName=?",userName)
+        if cursor.fetchone()[0] == password:
+            return True
+        else :
+            return False
