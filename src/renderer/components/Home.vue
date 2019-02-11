4f55555edf6d5083ee6ff6c26e1c382124dd150b
florimondmanca <florimond.manca@gmail.com>
2019-02-11 11:17:49 +0100

    Allow to export MIDI from menu

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -7,21 +7,11 @@
 
 <script>
   import SongEditor from './SongEditor/SongEditor.vue'
-  import store from '../store'
   import LoadDialog from './LoadDialog'
 
   export default {
     name: 'Home',
     components: {SongEditor, LoadDialog},
-    methods: {
-      save () {
-        this.$store.dispatch('MusicStore/getSavedTracks')
-      },
-      load () {
-        this.$store.dispatch('MusicStore/getSavedTracks')
-        this.showLoadDialog = true
-      }
-    },
     data () {
       return {
         showLoadDialog: false
@@ -29,11 +19,36 @@
     },
     mounted () {
       this.$electron.ipcRenderer.on('saving', () => {
-        store.dispatch('MusicStore/saveTrack')
+        this.$store.dispatch('MusicStore/saveTrack')
       })
       this.$electron.ipcRenderer.on('load', () => {
         this.load()
       })
+      // FIX: Electron may send the event multiple times, causing
+      // the file selection window to show up multiple times.
+      // So use `.once()` instead of `.on()` and rebind a once listener
+      // in `.exportMidi()`.
+      // See: (A)
+      this.$electron.ipcRenderer.once('exportMidi', this.exportMidi)
+    },
+    methods: {
+      save () {
+        this.$store.dispatch('MusicStore/getSavedTracks')
+      },
+      load () {
+        this.$store.dispatch('MusicStore/getSavedTracks')
+        this.showLoadDialog = true
+      },
+      exportMidi () {
+        // (A)
+        this.$store.dispatch('MusicStore/exportMidi')
+        // Wait a bit so that the extra Electron events that will be sent
+        // in a few ms are not caught.
+        setTimeout(
+          () => this.$electron.ipcRenderer.once('exportMidi', this.exportMidi),
+          100
+        )
+      }
     }
   }
 </script>
