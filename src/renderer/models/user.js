9489d455844ad81087dad3ed5f6f457e8ce6481f
florimondmanca <florimond.manca@gmail.com>
2019-02-16 18:02:50 +0100

    create and use an User class

---
+++ src/renderer/models/user.js
@@ -0,0 +1,7 @@
+export class User {
+  constructor ({ username, firstName, lastName }) {
+    this.username = username
+    this.firstName = firstName
+    this.lastName = lastName
+  }
+}
--- src/renderer/store/modules/auth.js
+++ src/renderer/store/modules/auth.js
@@ -1,15 +1,20 @@
 import http from '../../utils/http'
+import { User } from '@/models'
 
 const TOKEN_STORAGE_ITEM = 'polyphona-token'
 const USER_STORAGE_ITEM = 'polyphona-user'
 
 const getters = {
-  getUser: (state) => state.user
+  getUser: state => state.user
 }
 
 const mutations = {
   setUser (state, user) {
-    localStorage.setItem(USER_STORAGE_ITEM, JSON.stringify(user))
+    if (user) {
+      localStorage.setItem(USER_STORAGE_ITEM, JSON.stringify(user))
+    } else {
+      localStorage.removeItem(USER_STORAGE_ITEM)
+    }
     state.user = user
   },
   setToken (state, token) {
@@ -17,23 +22,32 @@ const mutations = {
     http.defaults.headers.common['Authorization'] = 'Token ' + token
     state.token = token
   },
-  discardToken (state) {
+  discardToken () {
     localStorage.removeItem(TOKEN_STORAGE_ITEM)
   }
 }
 
 const actions = {
-  async register (context, {username, firstName, lastName, password}) {
-    const payload = {username, first_name: firstName, last_name: lastName, password}
+  async register (context, { username, firstName, lastName, password }) {
+    const payload = {
+      username,
+      first_name: firstName,
+      last_name: lastName,
+      password
+    }
     await http.post('/users/', payload)
-    await context.dispatch('login', {username, password})
+    await context.dispatch('login', { username, password })
   },
-  login (context, {username, password}) {
-    return http.post('/tokens/', {username, password}).then((resp) => {
-      const {token, user} = resp.data
-      context.commit('setToken', token)
-      context.commit('setUser', user)
+  async login (context, { username, password }) {
+    const resp = await http.post('/tokens/', { username, password })
+    const { token, user: data } = resp.data
+    const user = new User({
+      username: data.username,
+      firstName: data.first_name,
+      lastName: data.last_name
     })
+    context.commit('setToken', token)
+    context.commit('setUser', user)
   },
   logout (context) {
     context.commit('discardToken')
@@ -48,8 +62,8 @@ if (token) {
   http.defaults.headers.common['Authorization'] = 'Token ' + token
 }
 const state = {
-  user: rawUser ? JSON.parse(rawUser) : null,
+  user: rawUser ? new User(JSON.parse(rawUser)) : null,
   token
 }
 
-export default {namespaced: true, getters, actions, mutations, state}
+export default { namespaced: true, getters, actions, mutations, state }
