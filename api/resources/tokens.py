bd5c37769118ea5e291795f91423b9567c44e38c
florimondmanca <florimond.manca@gmail.com>
2019-02-17 17:15:11 +0100

    add docstrings on tokens resource

--- api/resources/tokens.py
+++ api/resources/tokens.py
@@ -8,11 +8,23 @@ from .decorators import require_fields
 
 
 class TokenResource:
+    """Resource to create or delete tokens."""
+
     def __init__(self, db: Database):
         self.db = db
 
     @require_fields("username", "password")
     def on_post(self, req: Request, resp: Response):
+        """Exchange user credentials for a new token.
+        
+        The payload must contain the ``username`` and ``password`` fields.
+        
+        Raises
+        ------
+        falcon.HTTPUnauthorized
+            If the given credentials are invalid.
+        """
+
         username = req.media["username"]
         password = req.media["password"]
 
@@ -25,5 +37,6 @@ class TokenResource:
         resp.media = {"token": token, "user": self.db.get_user(username)}
 
     def on_delete(self, _, resp: Response, token: str):
+        """Delete a token."""
         self.db.delete_token(token)
         resp.status = falcon.HTTP_204
