800a6c02840b67966df20d8b2c9c9e505e7ef16c
florimondmanca <florimond.manca@gmail.com>
2019-02-17 17:16:34 +0100

    add docstrings of users resource

--- api/resources/users.py
+++ api/resources/users.py
@@ -7,11 +7,32 @@ from .decorators import require_fields
 
 
 class UserResource:
+    """Resource to create users.
+
+    Parameters
+    -----------
+    db : Database
+    """
+
     def __init__(self, db: Database):
         self.db = db
 
     @require_fields("username", "first_name", "last_name", "password")
     def on_post(self, req: Request, resp: Response):
+        """Create a new user.
+
+        Required fields:
+
+        - ``username``
+        - ``first_name``
+        - ``last_name``
+        - ``password``
+    
+        Raises
+        ------
+        HTTPBadRequest :
+            If a user named ``username`` already exists.
+        """
         username = req.media["username"]
 
         if not self.db.user_exists(username):
